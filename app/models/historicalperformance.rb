5447edfe367ed7d1e35802bd80fd0d18feabd1c1
Raghu Betina <raghu@firstdraft.com>
2016-12-01 19:39:20 +0000

    Validate uniqueness of historicalperformances.performance

--- app/models/historicalperformance.rb
+++ app/models/historicalperformance.rb
@@ -5,6 +5,8 @@ class Historicalperformance < ApplicationRecord
 
   # Validations
 
+  validates :performance, :uniqueness => { :scope => [:fund_id, :month] }
+
   validates :performance, :presence => true
 
   validates :performance, :numericality => true
