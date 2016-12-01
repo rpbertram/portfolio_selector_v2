5fbca1a76da3e325f3f5e006ba1addd00031e498
Raghu Betina <raghu@firstdraft.com>
2016-12-01 19:40:01 +0000

    Whitelist columns in Historicalperformance for ActiveAdmin

--- app/admin/historicalperformance.rb
+++ app/admin/historicalperformance.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Historicalperformance do
 
+ permit_params :fund_id, :month, :performance
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
