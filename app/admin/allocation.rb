b34253000f7f28bbb629319fd24c963658820fcb
Raghu Betina <raghu@firstdraft.com>
2016-12-01 19:39:54 +0000

    Whitelist columns in Allocation for ActiveAdmin

--- app/admin/allocation.rb
+++ app/admin/allocation.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Allocation do
 
+ permit_params :client_id, :fund_id, :percentage
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
