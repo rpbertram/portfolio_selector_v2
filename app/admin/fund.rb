3d11effd006e048ca4200af5544f822fec82893f
Raghu Betina <raghu@firstdraft.com>
2016-12-01 19:39:57 +0000

    Whitelist columns in Fund for ActiveAdmin

--- app/admin/fund.rb
+++ app/admin/fund.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Fund do
 
+ permit_params :name, :risktolerance, :proxyflag
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
